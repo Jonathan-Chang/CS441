749cb8457780a2dfaa4bc5f50364392b7e9ffe33
Jonathan <jonathan@Victors-MacBook-Air-2.local>
2019-01-26 00:43:45 -0500

    Started some code

--- p1_helloworld/p1_helloworld/ViewController.swift
+++ p1_helloworld/p1_helloworld/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//Provides services for the map app that you see
+import MapKit
 
+//Provides services for determining device geographic location
+import CoreLocation
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var location = CLLocationCoordinate2DMake(42.0893553, -75.96970490000001)
+        
+        var span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        var region = MKCoordinateRegion(center: location, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        
+        annotation.title = "Binghamton University"
+        
+        mapView.addAnnotation(annotation)
+        
+        
+        mapView.showsUserLocation = true
     }
 
 
