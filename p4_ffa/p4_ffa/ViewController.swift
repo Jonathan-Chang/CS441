568e5913e2b22542ef1428388892ac381a15ea57
Jonathan <jonathan@Victors-MacBook-Air-2.local>
2019-03-13 21:05:37 -0400

    ff

--- p4_ffa/p4_ffa/ViewController.swift
+++ p4_ffa/p4_ffa/ViewController.swift
@@ -12,6 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         int x = 5
+        
+        let gameRect: CGRect
+        override init(size: CGSize){
+            
+            
+            let maxRatio: CGFloat = 16.0/9.0
+            let playableWidth = size.height / maxRatio
+            let margin = (size.width - playableWidth) / 2
+            
+            gameRect = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+            super.init(size: size)
+            
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override func didMove(to view: SKView) {
+            
+            self.physicsWorld.contactDelegate = self
+            ironMan.setScale(10)
+            ironMan.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
+            ironMan.zPosition = 2
+            ironMan.physicsBody = SKPhysicsBody(rectangleOf: ironMan.size)
+            ironMan.physicsBody!.affectedByGravity = false
+            ironMan.physicsBody!.categoryBitMask = PhysicsCat.IronMan
+            ironMan.physicsBody!.collisionBitMask = PhysicsCat.None
+            ironMan.physicsBody!.contactTestBitMask = PhysicsCat.Enemy
+            
+            
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
