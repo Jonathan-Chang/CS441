bb15f297ae3f39a1a1715f5d0a84c88a3d62d14b
Kevin Wu <kevinwu@Kevins-MacBook-Air-4.local>
2019-04-05 17:39:46 -0400

    Extract

--- p5_teamffa/app/src/main/java/android/p5_teamffa/MainActivity.java
+++ p5_teamffa/app/src/main/java/android/p5_teamffa/MainActivity.java
@@ -1,10 +1,13 @@
 package android.p5_teamffa;
 
 import android.p5_teamffa.model.Feed;
+import android.p5_teamffa.model.entry.Entry;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -41,8 +44,28 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<Feed>() {
             @Override
             public void onResponse(Call<Feed> call, Response<Feed> response) {
-                Log.d(TAG, "onResponse: feed: " + response.body().toString());
+             //   Log.d(TAG, "onResponse: feed: " + response.body().toString());
                 Log.d(TAG, "onResponse: Server Response: " + response.toString());
+                List<Entry> entrys = response.body().getEntries();
+//                Log.d(TAG, "onResponse: entrys: " + response.body().getEntries());
+//                Log.d(TAG, "onResponse: author: " + entrys.get(0).getAuthor());
+//                Log.d(TAG, "onResponse: updated: " + entrys.get(0).getUpdate());
+//                Log.d(TAG, "onResponse: title: " + entrys.get(0).getTitle());
+                for(int i = 0; i < entrys.size(); i++) {
+                    ExtractXML extractXML1 = new ExtractXML(entrys.get(0).getContent(), "<a href=");
+                    List<String> postContent = extractXML1.start();
+
+                    ExtractXML extractXML2 = new ExtractXML(entrys.get(0).getContent(), "<img src=");
+                    try {
+                        postContent.add(extractXML2.start().get(0));
+                    }catch (NullPointerException e){
+                        postContent.add(null);
+                    }
+                    catch (IndexOutOfBoundsException e){
+                        postContent.add(null);
+                    }
+                    extractXML2.start();
+                }
             }
 
             @Override
